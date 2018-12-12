cd3b7d078aa537557f26e4ef730d9ded08afc43a
sm1996 <36611191+sm1996@users.noreply.github.com>
2018-12-12 19:14:24 +0000

    Added files

--- plantio_app.py
+++ plantio_app.py
@@ -17,13 +17,15 @@ def make_plant(name,plant_date):
   return plant
 
 rose=make_plant("rose",datetime(2018,12,05,0,0,0,tzinfo=UTC))
-grass=make_plant("grass",datetime(2018,12,06,0,0,0,tzinfo=UTC))
+fig=make_plant("fig",datetime(2018,12,06,0,0,0,tzinfo=UTC))
 aloe=make_plant("aloe",datetime(2018,12,07,0,0,0,tzinfo=UTC))
+sunflower=make_plant("sunflower",datetime(2018,12,07,0,0,0,tzinfo=UTC))
 
 plants_list=[]
 plants_list.append(rose)
-plants_list.append(grass)
+plants_list.append(fig)
 plants_list.append(aloe)
+plants_list.append(sunflower)
 
 user_plants=[]
 #user_calendar=ics.icalendar.Calendar()
@@ -38,32 +40,21 @@ def index():
 def plant_cal():
     input_plants = request.form.getlist('plants')
     #compare user plants to app's plant catalogue
+    cal = Calendar()
+    cal.add('prodid', '-//My calendar product//mxm.dk//')
+    cal.add('version', '2.0')
+
     for plant in plants_list:
         if (plant.name) in input_plants:
-            user_plants.append(plant)
-        for plant in user_plants:
-            #c=ics.icalendar.Calendar()
-            #e=ics.event.Event()
-            #e.name=plant.name+" Planting Date"
-            #e.begin=str(plant.plant_date).replace("-","")
-            #c.append(e)
-            #c.events
-            #print str(c)
-            cal = Calendar()
-            cal.add('prodid', '-//My calendar product//mxm.dk//')
-            cal.add('version', '2.0')
-
             event = Event()
             event.add('summary', plant.name)
             event.add('dtstart', plant.plant_date)
             event.add('dtend', plant.plant_date)
-            
             cal.add_component(event)
-
-            f = open('example.ics', 'wb')
-            f.write(cal.to_ical())
-            f.close()
-            print cal.to_ical()
+    f = open('your_plant_calendar.ics', 'wb')
+    f.write(cal.to_ical())
+    f.close()
+    print cal.to_ical()
     return render_template('plant_cal.html',user_plants=user_plants)
 
 app.run(debug=True)
